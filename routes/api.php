96484428f9ff55634e9f9fe82cf9353bd922c376
boodydahab <boodydahab98@gmail.com>
2022-07-23 23:20:54 +0200

    Update api.php

--- routes/api.php
+++ routes/api.php
@@ -1,7 +1,6 @@
 <?php
 
 use App\Http\Controllers\Api\BloodTypeApiController;
-use App\Http\Controllers\Api\ClientApiController;
 use App\Http\Controllers\Api\GovernaorateApiController;
 use App\Http\Controllers\Api\MainController;
 use Illuminate\Http\Request;
@@ -21,35 +20,31 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
-Route::group(['prefix' => 'v1'], function(){
-    Route::get('clients', [MainController::class,'client']);
-    Route::get('cities', [MainController::class,'cities']);
-    Route::get('posts', [MainController::class,'posts']);
-    Route::get('notifications', [MainController::class,'notifications']);
-    Route::get('settings', [MainController::class,'settings']);
-    Route::get('donation_requests', [MainController::class,'donation_requests']);
-    Route::get('client_governorates', [MainController::class,'client_governorates']);
+Route::group(['prefix' => 'v1'], function () {
+    Route::get('clients', [MainController::class, 'client']);
+    Route::get('cities', [MainController::class, 'cities']);
+    Route::get('posts', [MainController::class, 'posts']);
+    Route::get('notifications', [MainController::class, 'notifications']);
+    Route::get('settings', [MainController::class, 'settings']);
+    Route::get('donation_requests', [MainController::class, 'donation_requests']);
+    Route::get('client_governorates', [MainController::class, 'client_governorates']);
     //
-    Route::prefix('governorates')->group(function(){
-        Route::get('/', [GovernaorateApiController::class,'index']);
-        Route::get('/{governoratesId}', [GovernaorateApiController::class,'show']);
-        Route::post('/', [GovernaorateApiController::class,'create']);
-        Route::put('/{governoratesId}', [GovernaorateApiController::class,'update']);
-
+    Route::prefix('governorates')->group(function () {
+        Route::get('/', [GovernaorateApiController::class, 'index']);
+        Route::get('/{governoratesId}', [GovernaorateApiController::class, 'show']);
+        Route::post('/', [GovernaorateApiController::class, 'create']);
+        Route::put('/{governoratesId}', [GovernaorateApiController::class, 'update']);
     });
 
-
-
-    Route::prefix('bloodtype')->group(function(){
-        Route::get('/', [BloodTypeApiController::class,'index']);
-        Route::get('/{bloodtypeId}', [BloodTypeApiController::class,'show']);
-        Route::post('/', [BloodTypeApiController::class,'create']);
-        Route::put('/{bloodtypeId}', [BloodTypeApiController::class,'update']);
-
+    Route::prefix('bloodtype')->group(function () {
+        Route::get('/', [BloodTypeApiController::class, 'index']);
+        Route::get('/{bloodtypeId}', [BloodTypeApiController::class, 'show']);
+        Route::post('/', [BloodTypeApiController::class, 'create']);
+        Route::put('/{bloodtypeId}', [BloodTypeApiController::class, 'update']);
     });
 
-    Route::get('client_posts', [MainController::class,'client_posts']);
-    Route::get('client_notifications', [MainController::class,'client_notifications']);
-    Route::get('contacts', [MainController::class,'contacts']);
-    Route::get('blood_type_clients', [MainController::class,'blood_type_clients']);
+    Route::get('client_posts', [MainController::class, 'client_posts']);
+    Route::get('client_notifications', [MainController::class, 'client_notifications']);
+    Route::get('contacts', [MainController::class, 'contacts']);
+    Route::get('blood_type_clients', [MainController::class, 'blood_type_clients']);
 });
